d5230d1ab8d26cb2f4f31b04a59e28209d1d2a59
jungjun <hojanrgid@gmail.com>
2019-11-21 13:47:40 +0900

    19.11.21

--- Algorithm/stkque4.cpp
+++ Algorithm/stkque4.cpp
@@ -1,45 +1,42 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int bl = 2;
-int w = 10;
-vector<int> t_w = {7,4,5,6};
-
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    
-    int time = 0;
-    int bw = 0;
-    queue<int> twq;
-    queue<int> bwq;
+    int answer = 0;
+    int bridge_weight = 0;
+    queue< pair<pair<int,int>,int> > bridge_queue;
+    queue< int > truck_queue;
     
     for(int i=0; i<truck_weights.size(); i++) {
-        twq.push(truck_weights[i]);
+        truck_queue.push(truck_weights[i]);
     }
     
-    while(!twq.empty() || !bwq.empty()) {
-        time++;
-        if(time%(bridge_length+1) == 0) {
-            if(!bwq.empty()) {
-                bw -= bwq.front();
-                bwq.pop();
-            }
+    while(!truck_queue.empty() || !bridge_queue.empty()) {
+        answer++;
+        while(!bridge_queue.empty()) {
+            int cw = bridge_queue.front().first.first;
+            int cp = bridge_queue.front().first.second;
+            int ct = bridge_queue.front().second;
+            if(ct == answer) break;
+            bridge_queue.pop();
+            
+            cp += 1;
+            if(cp == bridge_length+1) {
+                bridge_weight -= cw;
+                continue;
+            }  
+            bridge_queue.push(make_pair(make_pair(cw,cp),ct+1));
         }
-        
-        if(bw + twq.front() <= weight) {
-            bw += twq.front();
-            bwq.push(twq.front());
-            twq.pop();
+        if(truck_queue.empty()) continue;
+        if(bridge_weight + truck_queue.front() <= weight) {
+            bridge_weight += truck_queue.front();
+            bridge_queue.push(make_pair(make_pair(truck_queue.front(),1),answer));
+            truck_queue.pop();
         }
-        
     }
-    return time;
-}
-
-int main(void) {
-    int answer = 0;
-    answer = solution(bl,w,t_w);
-    printf("answer:%d\n", answer);
+    return answer;
 }
